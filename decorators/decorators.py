cfe7c78f2b320517f60c6497b8f050a42a8f7aed
Jagjeet Singh <jagjeetsingh@Jagjeets-Mac.local>
2024-05-03 07:56:03 +0530

    decorator file

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -1,16 +1,16 @@
 import time 
 
-def timer(function):
+def timer(function): #timer is a function which takes another function as a parameter  
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = function(*args, **kwargs)
         end_time = time.time()
         print(f"{function.__name__} ran in {end_time - start_time}")
-        return result
-    return wrapper 
+        # return result
+    return wrapper
 
 @timer
 def example_function(n):
     time.sleep(n)
-
-example_function(5)
+    
+example_function(2)
